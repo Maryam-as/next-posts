9b64c656507d3221570211f4f0d5d5d893287384
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2026-05-31 19:29:41 +0330

    feat: provide form submission feedback with useFormStatus

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
 
 import { storePost } from "@/lib/posts";
+import FormSubmit from "@/components/form-submit";
 
 export default function NewPostPage() {
   async function createPost(formData) {
@@ -36,8 +37,7 @@ export default function NewPostPage() {
           <textarea id="content" name="content" rows="5" />
         </p>
         <p className="form-actions">
-          <button type="reset">Reset</button>
-          <button>Create Post</button>
+          <FormSubmit />
         </p>
       </form>
     </>
